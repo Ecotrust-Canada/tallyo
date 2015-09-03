8cf2de3f5f7ccb6269ae44d2f56ebe5c9517ff90
Alex <alex@ecotrust.ca>
2015-09-03 16:22:58 -0700

    html for amanda

--- public/js/base.js
+++ public/js/base.js
@@ -8,9 +8,11 @@ HarsamApp.config(['$routeProvider',
       when('/home', {
         templateUrl: 'home.html'
       }).
+      when('/harsam',{
+        templateUrl: 'harsam.html'
+      }).
       when('/harsam_admin', {
-        templateUrl: 'harsam_admin.html',
-        //controller: 'InProductionCtrl'
+        templateUrl: 'harsam_admin.html'
       }).
       when('/harsam2', {
         templateUrl: 'harsam2.html',
@@ -20,8 +22,25 @@ HarsamApp.config(['$routeProvider',
         templateUrl: 'harsam1.html',
         controller: 'ReceivingCtrl'
       }).
+      when('/amanda',{
+        templateUrl: 'amanda.html'
+      }).
+      when('/amanda_admin', {
+        templateUrl: 'amanda_admin.html'
+      }).
+      when('/amanda2', {
+        templateUrl: 'amanda2.html',
+        controller: 'SelectLotCtrl'
+      }).
+      when('/amanda1', {
+        templateUrl: 'amanda1.html',
+        controller: 'ReceivingCtrl'
+      }).
       otherwise({
         redirectTo: '/home'
       });
 
   }]);
+
+
+
