8d8eb5bc56a4f8e865e6fc117bf7a4ce7fbc3ef0
Alex <alex@ecotrust.ca>
2015-09-09 17:54:58 -0700

    working on functionality for revised harsam schema, with as much abstraction as possible

--- public/js/common.js
+++ public/js/common.js
@@ -1,63 +1,53 @@
-var dateManipulation = function(date){
-    var dates = {
-    'postgres_date': moment(date.valueOf()).format(),
-    'start_date': moment(date.valueOf()).startOf('isoWeek'),
-    'end_date': moment(date.valueOf()).endOf('isoWeek')
-    };
+'use strict';
+
+/*period defaults to day*/
+var dateManipulation = function(date, period){
+    if (period !== 'day' && period !== 'week'){
+        period = 'day';
+    }
+    var dates;
+    if (period === 'week'){
+        dates = {
+        'postgres_date': moment(date).format(),
+        'start_date': moment(date).startOf('isoWeek'),
+        'end_date': moment(date).endOf('isoWeek')
+        };
+    }
+    else if (period === 'day'){
+        dates = {
+        'postgres_date': moment(date).format(),
+        'start_date': moment(date).startOf('day'),
+        'end_date': moment(date).endOf('day')
+        };
+    }
     return dates;
-}
+};
 
-var createLotNum = function(date, sup_id){
-    var datestring = moment(date.valueOf()).format('DDMMYYYY');
-    var lot_num = (String(sup_id)).concat(datestring);
-    return lot_num;
-}
-
-var CreateProductionLotNumber = function(prod_id, receive_lot, date){
-    var datestring = moment(date.valueOf()).format('DDMMYYYY');
-    var lot_num = (String(receive_lot).substring(0,5)).concat(String(prod_id)).concat(datestring);
-    return lot_num;
-}
+/* for now creates unique id by using stage id and current date and time*/
+var createLotNum = function(stage_id, date){
+    var datestring = moment(date.valueOf()).format('-DDMMYYYY-HHmmss');
+    return String(stage_id) + datestring;
+};
 
 
+var LotQuery = function(params){
+    var queryString = '?stage_id=eq.' + params.stage_id;
 
+    queryString = queryString + ['supplier_id','previous_lot_number','product_id','lot_number'].map(function(param){
+      if (typeof params[param] !== 'undefined'){
+        return '&' + param + '=eq.' + params[param];
+      }
+      else{
+        return '';
+      }      
+    }).join('');
 
-
-
-
-var InitReceivingLotEntry = function(plant_settings){
-    var jsonobj = {
-        'lot_number': '',
-        'id_supplier': '',
-        'in_prod': 'false',
-        'start_date': '',
-        'end_date': ''
-    };
-    return jsonobj;
-}
-
-
-var InitReceivingEntry = function(plant_settings){
-    var jsonobj = {
-        'weight': '',
-        'timestamp': '',
-        'lot_number_receiving_lots': '',
-        'end_date': ''
-    };
-    if (plant_settings.grade === 'true'){
-        jsonobj.grade = '';
+    if (typeof params.date !== 'undefined'){
+      var date = moment(params.date).format();
+      queryString = queryString.concat('&start_date=lt.' + date + '&end_date=gt.' + date);
     }
-    return jsonobj;
-}
-
-
-var CreateReceivingLotQuery = function(plant_settings, re_lot_entry, date){
-    var url = plant_settings.database_url;
-    url = url.concat('/receiving_lots?');
-    if (plant_settings.re_query_date_in_range === 'true'){
-        url.concat('&start_date=lt.' + date + '&end_date=gt.' + date)
-    }
-}
+    return queryString;
+  };
 
 
 
