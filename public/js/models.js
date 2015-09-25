cdba6ae10f04f312f72af838aa8a430eb434541f
Oleg Matvejev <omatveje@gmail.com>
2015-09-24 18:05:23 -0700

    Implement Model structure for angular.
    We will not be using this structure.

--- public/js/models.js
+++ public/js/models.js
@@ -2,100 +2,117 @@
 
 var app = angular.module('scanthisApp.shared', []);
 
-
-app.factory('EntryManager', ['$http', '$q', 'Entry', function($http, $q, Entry) {
-  var EntryManager = {
-    _pool: {},
-    _url: 'http://10.10.50.30:3000/entry',
-
-    // Private Functions
-    _retrieveInstance(entryId, entryData) {
-      var instance = this._pool[entryId];
-
-      if (instance) {
-        instance.setData(entryData);
-      }
-      else {
-        instance = new Entry(entryData);
-        this._pool[entryId] = instance;
-      }
-
-      return instance;
-    },
-
-    _search(entryId) {
-      return this._pool[entryId];
-    },
-
-    _load(entryId, deferred) {
-      var self = this;
-
-      $http.get(self._url + '?id=eq.' + entryId)
-        .success(function(entryData) {
-          var entry = self._retrieveInstance(entryData.id, entryData);
-          deferred.resolve(entry);
-        })
-        .error(function() {
-          deferred.reject();
-        });
-    },
-
-    // Public Functions
-    getEntry(entryId) {
-      var deferred = $q.defer();
-      var entry = this._search(entryId);
-
-      if (entry) {
-        deferred.resolve(entry);
-      }
-      else {
-        this._load(entryId, deferred);
-      }
-      return deferred.promise;
-    },
-
-    loadAllEntries() {
-      var deferred = $q.defer();
-      var self = this;
-      $http.get(this._url)
-        .success(function(entriesArray) {
-          var entries = [];
-          entriesArray.forEach(function(entryData) {
-            var entry = self._retrieveInstance(entryData.id, entryData);
-            entries.push(entry);
-          });
-
-          deferred.resolve(entries);
-        })
-        .error(function() {
-          deferred.reject();
-        });
-      return deferred.promise;
-    },
+app.factory('getManager', ['ModelManager', function(ModelManager) {
+  var _managerList = {};
+  var getManager = function(Model) {
+    let name = Model.name;
+    if (!_managerList[name]) {
+      _managerList[name] = new ModelManager(Model);
+    }
+    return _managerList[name];
   };
-
-  return EntryManager;
+  return getManager;
 }]);
 
-app.factory('Entry', ['$http', function($http) {
-  class Entry {
-    constructor(entryData, url) {
-      this.setData(entryData);
-      this.url = 'http://10.10.50.30:3000/entry?id=eq.';
+app.service('ModelManager', ['$http', '$q', function($http, $q) {
+
+  class ModelManager {
+    constructor(Model) {
+      this.Model = Model;
+      this.url = Model.url();
     }
 
-    setData(entryData) {
-      angular.extend(this, entryData);
+    _createModelsList(array) {
+      var entries = [];
+      var self = this;
+      array.forEach(function(data) {
+        var entry = new self.Model(data);
+        entries.push(entry);
+      });
+      return entries;
     }
 
-    delete() {
-      $http.delete(this.url + this.id);
+    all() {
+      var deferred = $q.defer();
+      var self = this;
+      $http.get(this.url)
+        .success(function(list) {
+          deferred.resolve(self._createModelsList(list));
+        })
+        .error(function() {
+          deferred.reject();
+        });
+      return deferred.promise;
     }
 
-    update() {
-      $http.put(this.url + this.id, this);
+    filter(queryset) {
+      var deferred = $q.defer();
+      var self = this;
+      $http.get(this.url + queryset) 
+        .success(function(list) {
+          deferred.resolve(self._createModelsList(list));
+        })
+        .error(function() {
+          deferred.reject();
+        });
+      return deferred.promise;
     }
   }
-  return Entry;
+
+  return ModelManager;
 }]);
 
+app.service('BaseModel', ['$http', '$q', 'getManager', function($http, $q, getManager) {
+  class BaseModel {
+    constructor(data) {
+      this.setData(data);
+      this.url = this.constructor.url();
+    }
+
+    setData(data) {
+      angular.extend(this, data);
+    }
+  
+    delete() {
+      $http.delete(this.url + '?id=eq.' + this.id);
+    }
+  
+    update() {
+      $http.put(this.url + '?id=eq.' + this.id, this);
+    }
+  
+    save() {
+      $http.post(this.url, this);
+    }
+
+    static objects() {
+      var Model = this.prototype.constructor;
+      return getManager(Model);
+    }
+
+    static url() {
+      throw "You need to define static url() method in your model";
+    }
+  }
+  return BaseModel;
+}]);
+
+app.factory('Item', ['BaseModel', function(BaseModel) {
+  class Item extends BaseModel {
+    static url() {
+      return "http://10.10.50.30:3000/item";
+    }
+  }
+
+  return Item;
+}]);
+
+app.factory('Lot', ['BaseModel', function(BaseModel) {
+  class Lot extends BaseModel {
+    static url() {
+      return "http://10.10.50.30:3000/lot";
+    }
+  }
+
+  return Lot;
+}]);
