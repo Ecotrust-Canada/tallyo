f6c9b49e21d8a87c392a805512ab024aeb15c469
Alex <alex@ecotrust.ca>
2015-11-10 16:40:55 -0800

    deleting old directives

--- public/js/directives.js
+++ public/js/directives.js
@@ -3,23 +3,12 @@
 
 angular.module('scanthisApp.directives', [])
 
-/*button to switch from makeitem to summaryblock*/
-.directive('viewsummarybutton', function() { return { templateUrl: 'htmlpartials/viewsummarybutton.html' }; })
 
 /*select lot from dropdown*/
 .directive('selectlot', function() { return { templateUrl: 'htmlpartials/selectlot.html' }; })
 
-/*creating item entries*/
-.directive('makeitem', function() { return { templateUrl: 'htmlpartials/makeitem.html' }; })
-
-/*summary of items table*/
-.directive('summaryblock', function() { return { templateUrl: 'htmlpartials/summaryblock.html' }; })
-
 /*form with submit button*/
 .directive('createform', function() { return { templateUrl: 'htmlpartials/createform.html' }; })
-
-/*moving lots from one stage to another*/
-.directive('movelot', function() { return { templateUrl: 'htmlpartials/movelot.html' }; })
 
 /*table with suppliers with button to set as current*/
 .directive('supplierlist', function() { return { templateUrl: 'htmlpartials/supplierlist.html' }; })
