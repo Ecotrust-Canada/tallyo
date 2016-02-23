8890cc05fead91d5a813c152bdddf6503bf59e80
Marina Landisberg <mlandisberg@gmail.com>
2016-02-23 01:26:01 -0800

    starting with screen for 'Receiving' station for HarSam, moving loadCurrent to StationCtrl

--- public/js/base.js
+++ public/js/base.js
@@ -65,7 +65,7 @@ angular.module('scanthisApp', [
   }
 })
 
-.controller('StationCtrl', function($scope, $http, $sce) {
+.controller('StationCtrl', function($scope, $http, $sce, DatabaseServices) {
 
   $scope.init = function(settings){
     $scope.station_code = settings.station_code;
@@ -124,7 +124,7 @@ angular.module('scanthisApp', [
           data: {data:$scope.printString(codeString, fieldarray)}
 
         });
-  };
+      };
     }
     
     if(settings.packingconfig){
@@ -165,7 +165,25 @@ angular.module('scanthisApp', [
     }
 
     //$scope.showsection = "before";
+    if ($scope.options.loadcurrentcollection) {
+        $scope.loadCurrent();
+    }
   };
+
+  $scope.loadCurrent = function(){
+    var func = function(response){
+      var station = response.data[0];
+      var today = moment(new Date()).startOf('day').format();
+      if(station){
+        if (moment(station.in_progress_date).startOf('day').format() === today){
+          $scope.current.collectionid = station.lot_number;
+        }
+      }
+    };
+    var query = '?station_code=eq.' + $scope.station_code + '&in_progress=eq.true';
+    DatabaseServices.GetEntries('lotlocations', func, query);
+  };
+
 })
 
 ;
