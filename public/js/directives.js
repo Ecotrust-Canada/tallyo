89158a6a7d209f07b9b02236e60143c1fecb1c19
alex <alex@ecotrust.ca>
2016-05-03 13:42:48 -0700

    fix bufferredscroll to continue loading on scroll down

--- public/js/directives.js
+++ public/js/directives.js
@@ -198,6 +198,13 @@ angular.module('scanthisApp.directives', [])
             scrollHeight = element[0].scrollHeight,
             offsetHeight = element[0].offsetHeight;
 
+            console.log(scrollTop, scrollHeight, offsetHeight);
+
+        if (scrollTop === (scrollHeight - (offsetHeight-17))) {
+          $scope.$apply(function () {
+            handler($scope);
+          });
+        }
         if (scrollTop === (scrollHeight - offsetHeight)) {
           $scope.$apply(function () {
             handler($scope);
