87ac72333308b2736c827d3208138e7819f1872f
Clark Van Oyen <clark@countable.ca>
2016-05-09 16:33:59 +0700

    lots of cleanup

---
+++ deploy/backups/run_backups.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# Back up the database once per hour, to the /bak/folder with filename showing the current datetime.
+
+mkdir -p /bak
+
+export PGPASSWORD=postgres
+
+FILENAME=/bak/tallyo-$(date +%Y%m%d-%H%M%S).sql
+
+echo "Backing up..."
+
+pg_dump -h db -U tuna_processor postgres > $FILENAME
+
+echo "A backup was saved in $FILENAME"
+
+# remove anything that's not the first of a month, and is at least a month old
+rm `find /bak/* -mtime "+30" | grep -v "01\-"` 2> /dev/null
+
+# remove files older than 2 days that aren't at 8 o'clock
+rm `find /bak/* -mtime "+2" | grep -v "\-22"` 2> /dev/null
+
+sleep 3600
