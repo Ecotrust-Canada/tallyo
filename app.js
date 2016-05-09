c150d4c47172ca2c3a64203026489b1a474d92e2
Hotfix <hotfix@tallyo.amanda.com>
2016-05-09 02:43:20 +0000

    remove warmups, no longer needed

--- app.js
+++ app.js
@@ -73,16 +73,6 @@ app.use(function(err, req, res, next) {
   });
 });
 
-setInterval(function(){
-/*  http.request({
-    host:'postgrest',
-    port:'3000',
-    path:'/'
-  }, function(){
-
-  }).end();*/
-}, 5000);
-
 //console.log(process.argv);
 module.exports = app;
 
