867660c506d4864f49e8f972cdef4a15eaf068b7
alex <alex@ecotrust.ca>
2016-05-11 22:18:12 -0700

    retouchign changes

--- migrations/sqls/20160512011837-somemorecsv-down.sql
+++ migrations/sqls/20160512011837-somemorecsv-down.sql
@@ -1,3 +1,57 @@
+-- View: public.totals_by_lot
+
+DROP VIEW public.totals_by_lot;
+
+CREATE OR REPLACE VIEW public.totals_by_lot AS 
+ SELECT scan.lot_number,
+    scan.station_code,
+    sum(scan.weight_1) AS weight_1,
+    scan.grade,
+    scan.species,
+    NULL::character varying AS size,
+    sum(scan.pieces) AS pieces,
+    NULL::bigint AS boxes
+   FROM scan
+  WHERE scan.lot_number IS NOT NULL AND scan.lot_number::text <> ''::text AND scan.loin_number IS NULL AND scan.box_number IS NULL
+  GROUP BY scan.lot_number, scan.station_code, scan.grade, scan.species
+UNION
+ SELECT box_with_info.lot_number,
+    box_with_info.station_code,
+    sum(box_with_info.weight) AS weight_1,
+        CASE
+            WHEN box_with_info.grade IS NOT NULL THEN box_with_info.grade
+            ELSE box_with_info.product_type
+        END AS grade,
+    NULL::character varying AS species,
+    box_with_info.size,
+    sum(box_with_info.pieces) AS pieces,
+    count(box_with_info.box_number) AS boxes
+   FROM box_with_info
+  WHERE box_with_info.lot_number IS NOT NULL AND box_with_info.lot_number::text <> ''::text
+  GROUP BY box_with_info.lot_number, box_with_info.station_code, box_with_info.grade, box_with_info.product_type, box_with_info.size
+UNION
+ SELECT loin_with_info.lot_number,
+    loin_with_info.station_code,
+    sum(loin_with_info.weight_1) AS weight_1,
+    loin_with_info.grade,
+    NULL::character varying AS species,
+    NULL::character varying AS size,
+    count(loin_with_info.loin_number) AS pieces,
+    NULL::bigint AS boxes
+   FROM loin_with_info
+  WHERE loin_with_info.lot_number IS NOT NULL AND loin_with_info.lot_number::text <> ''::text AND NOT (loin_with_info.station_code::text IN ( SELECT scan.station_code
+           FROM scan
+          WHERE scan.box_number IS NOT NULL))
+  GROUP BY loin_with_info.lot_number, loin_with_info.station_code, loin_with_info.grade
+  ORDER BY 4;
+
+ALTER TABLE public.totals_by_lot
+  OWNER TO tuna_processor;
+
+
+
+
+
 DROP VIEW public.scan_detail;
 
 CREATE OR REPLACE VIEW public.scan_detail AS 
--- migrations/sqls/20160512011837-somemorecsv-up.sql
+++ migrations/sqls/20160512011837-somemorecsv-up.sql
@@ -176,3 +176,57 @@ CREATE OR REPLACE VIEW public.scan_detail AS
 
 ALTER TABLE public.scan_detail
   OWNER TO tuna_processor;
+
+
+  -- View: public.totals_by_lot
+
+DROP VIEW public.totals_by_lot;
+
+CREATE OR REPLACE VIEW public.totals_by_lot AS 
+ SELECT scan.lot_number,
+    scan.station_code,
+    sum(scan.weight_1) AS weight_1,
+    scan.grade,
+    scan.species,
+    scan.state,
+    NULL::character varying AS size,
+    sum(scan.pieces) AS pieces,
+    NULL::bigint AS boxes
+   FROM scan
+  WHERE scan.lot_number IS NOT NULL AND scan.lot_number::text <> ''::text AND scan.loin_number IS NULL AND scan.box_number IS NULL
+  GROUP BY scan.lot_number, scan.station_code, scan.grade, scan.species, scan.state
+UNION
+ SELECT box_with_info.lot_number,
+    box_with_info.station_code,
+    sum(box_with_info.weight) AS weight_1,
+        CASE
+            WHEN box_with_info.grade IS NOT NULL THEN box_with_info.grade
+            ELSE box_with_info.product_type
+        END AS grade,
+    NULL::character varying AS species,
+    null as state,
+    box_with_info.size,
+    sum(box_with_info.pieces) AS pieces,
+    count(box_with_info.box_number) AS boxes
+   FROM box_with_info
+  WHERE box_with_info.lot_number IS NOT NULL AND box_with_info.lot_number::text <> ''::text
+  GROUP BY box_with_info.lot_number, box_with_info.station_code, box_with_info.grade, box_with_info.product_type, box_with_info.size
+UNION
+ SELECT loin_with_info.lot_number,
+    loin_with_info.station_code,
+    sum(loin_with_info.weight_1) AS weight_1,
+    loin_with_info.grade,
+    NULL::character varying AS species,
+    null as state,
+    NULL::character varying AS size,
+    count(loin_with_info.loin_number) AS pieces,
+    NULL::bigint AS boxes
+   FROM loin_with_info
+  WHERE loin_with_info.lot_number IS NOT NULL AND loin_with_info.lot_number::text <> ''::text AND NOT (loin_with_info.station_code::text IN ( SELECT scan.station_code
+           FROM scan
+          WHERE scan.box_number IS NOT NULL))
+  GROUP BY loin_with_info.lot_number, loin_with_info.station_code, loin_with_info.grade
+  ORDER BY 4;
+
+ALTER TABLE public.totals_by_lot
+  OWNER TO tuna_processor;
