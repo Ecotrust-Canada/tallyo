1281cc7bbe131bd89a8b5953a48115f6a5bf9103
alex <alex@ecotrust.ca>
2016-09-20 14:15:44 -0700

    add column species to loin

--- migrations/sqls/20160920204242-batchcode-down.sql
+++ migrations/sqls/20160920204242-batchcode-down.sql
@@ -1,2 +1,3 @@
 alter table box drop column batchcode;
 alter table box drop column pdc;
+alter table loin drop column species;
--- migrations/sqls/20160920204242-batchcode-up.sql
+++ migrations/sqls/20160920204242-batchcode-up.sql
@@ -1,2 +1,3 @@
 alter table box add column batchcode varchar;
 alter table box add column pdc date;
+alter table loin add column species varchar;
