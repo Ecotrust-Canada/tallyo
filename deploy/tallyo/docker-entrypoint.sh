84770d4806ee579bbe406de3b6e219107b33e51f
Michael Bryniarski <michael.bryniarski@gmail.com>
2016-10-02 20:34:09 -0700

    Set timezone in tallyo entrypoint

--- deploy/tallyo/docker-entrypoint.sh
+++ deploy/tallyo/docker-entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+echo "Setting timezone to :"$DB_TIMEZONE
+
+cp /usr/share/zoneinfo/$DB_TIMEZONE /etc/localtime
+echo $DB_TIMEZONE > /etc/timezone
+
 if [ $DB_RUNMIGRATIONS = true ] ; then
 	echo "Running database migrations"
 	db-migrate up -e env
