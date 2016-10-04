5a3f119a5eec3f9f48302c6bf5aafd9fe8b012d2
Steven Moon <steven@ecotrustcanada.local>
2016-10-04 23:33:18 +0700

    Added uuid support to db Dockerfile but it does not work and I had to create extension manually - needs fixed - TALLYO-540 Added migrations mount to dev docker-compose file - TALLYO-540 created new UUID datamatrix label for new AM2 Packing process - TALLYO-540

---
+++ migrations/sqls/20161004075752-add-box-uuid-from-label-down.sql
@@ -0,0 +1,2 @@
+/* Replace with your SQL commands */
+ALTER TABLE public.box DROP COLUMN uuid_from_label;
---
+++ migrations/sqls/20161004075752-add-box-uuid-from-label-up.sql
@@ -0,0 +1,2 @@
+/* Replace with your SQL commands */
+ALTER TABLE public.box ADD COLUMN uuid_from_label uuid;
