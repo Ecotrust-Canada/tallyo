fda017886e545112fd187393201d8b6e59932cf1
alex <alex@ecotrust.ca>
2016-10-06 10:47:53 -0700

    close dropdowns AND scan spaces

--- public/js/base.js
+++ public/js/base.js
@@ -47,6 +47,20 @@ angular.module('scanthisApp', [
   };
   $scope.stations = stationlist;
   $scope.terminals = terminals;
+
+  $scope.unFocus = function(){
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent)) {
+      var active = document.activeElement;
+      if (active.nodeName !== 'SELECT'){
+        active.blur();
+        var el = document.getElementById('to_focus');
+        if (el){
+          //alert('hmm');
+          el.focus();
+        }
+      }
+    }
+  };
 })
 
 .controller('RoutingCtrl', function($scope, $routeParams, $rootScope) {
