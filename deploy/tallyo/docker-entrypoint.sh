255088b0c68b9b08412335989d675ff2f8aeaa9a
Steven Moon <steven@ecotrustcanada.local>
2016-10-19 10:43:58 -0700

    updated tallyo dockerfile and entrypoint script to be tolorant of running stand-alone and produce errors on startup

--- deploy/tallyo/docker-entrypoint.sh
+++ deploy/tallyo/docker-entrypoint.sh
@@ -5,13 +5,26 @@ echo "Setting timezone to: "$TALLYO_TIMEZONE
 cp /usr/share/zoneinfo/$TALLYO_TIMEZONE /etc/localtime
 echo $TALLYO_TIMEZONE > /etc/timezone
 
-/wait-for-it.sh -t 0 $POSTGREST_HOSTNAME:3000
+if [ -n "$POSTGREST_HOSTNAME" ] ; then
+	echo "waiting for Postgrest..."
+	/wait-for-it.sh -t 0 $POSTGREST_HOSTNAME:3000
+else
+    echo "POSTGREST_HOSTNAME environment variable not found!"
+fi
 
 if [ $DB_RUNMIGRATIONS = true ] ; then
 	echo "Running database migrations"
 	db-migrate up -e env
 fi
 
-echo "Starting Node Server"
+if [ "$1" = "/bin/bash" ]; then
+    echo "Executing $1 instead of node based on first argument..."
+    exec "$1"
+elif [ "$1" = "app" ]; then
+    echo "Starting Node Server"
+    exec node "$@"
+else
+    echo "No valid arguments found. Exiting..."
+fi
 
-exec node "$@" 
+
